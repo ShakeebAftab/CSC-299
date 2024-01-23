c972e4243bdcc326a2d2528b17b98c36e67bb4de
shakeebaftab <shakeebaftab@gmail.com>
2024-01-23 14:13:42 -0600

    Fix

--- blink.py
+++ blink.py
@@ -1,11 +1,10 @@
 from gpiozero import LED
 
 led = LED(18)
-led.blink(0.5, 0.5, background=True)
 
-while True:
-  print("Notice that control has moved away - hit Enter to continue")
-  input()
-  print("Control is now back")
-  led.off()
-  input()
+led.blink(0.5, 0.5, background=True)
+print("Notice that control has moved away - hit Enter to continue")
+input()
+print("Control is now back")
+led.off()
+input()
