eb055917d1b359081c815ec675711332d7fb8ea8
shakeebaftab <shakeebaftab@gmail.com>
2024-01-30 14:18:03 -0600

    Recepie 13.2

---
+++ 13.2.py
@@ -0,0 +1,15 @@
+from gpiozero import Button, LED
+from time import sleep
+
+led = LED(23)
+
+def toggle_led():
+    print("toggling")
+    led.toggle()
+
+button = Button(18)
+button.when_pressed = toggle_led
+
+while True:
+    print("Busy doing other stuff")
+    sleep(2)
