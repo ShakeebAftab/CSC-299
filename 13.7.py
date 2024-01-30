edbe3e95c21d9b03321e313a834cb534a735e855
shakeebaftab <shakeebaftab@gmail.com>
2024-01-30 14:37:21 -0600

    Recepie 13.7

---
+++ 13.7.py
@@ -0,0 +1,25 @@
+from gpiozero import Button
+import time
+
+input_A = Button(18)
+input_B = Button(23)
+
+old_a = True
+old_b = True
+
+def get_encoder_turn():
+    # return -1 (cce), 0 (no movement), or +1 (cw)
+    global old_a, old_b
+    result = 0
+    new_a = input_A.is_pressed
+    new_b = input_B.is_pressed
+    if new_a != old_a or new_b != old_b :
+        if old_a == 0 and new_a == 1 :
+            result = (old_b * 2 - 1)
+        elif old_b == 0 and new_b == 1 :
+            result = -(old_a * 2 - 1)
+    old_a, old_b = new_a, new_b
+    time.sleep(0.001)
+    return result
+
+x = 0
