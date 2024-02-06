fdb0b32faa671da742d8a827db32ff8f0cc23a6b
shakeebaftab <shakeebaftab@gmail.com>
2024-02-06 14:49:07 -0600

    PIR Camera Module

---
+++ PIRCam.py
@@ -0,0 +1,27 @@
+from picamera import PiCamera
+from gpiozero import MotionSensor
+import time
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+
+pir_sensor = MotionSensor(4)
+
+def capture_image():
+    timestamp = time.strftime("%Y%m%d_%H%M%S")
+    image_filename = f"motion_capture_{timestamp}.jpg"
+    camera.capture(image_filename)
+    print(f"Image captured: {image_filename}")
+
+try:
+    while True:
+        pir_sensor.wait_for_motion()
+        print("Motion detected!")
+        capture_image()
+        time.sleep(5)
+
+except KeyboardInterrupt:
+    print("Exiting program.")
+finally:
+    camera.close()
+    pir_sensor.close()
