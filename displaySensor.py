76e43801b5a4bbddbdf20ef4d33b3eef0aab3bf2
shakeebaftab <shakeebaftab@gmail.com>
2024-02-13 15:47:17 -0600

    Display Sensor

--- displaySensor.py
+++ displaySensor.py
@@ -2,14 +2,12 @@ import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 from time import sleep
-from datetime import datetime
 from gpiozero import DistanceSensor
 
 # Set up display
 i2c = board.I2C()
 disp = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C)
 small_font = ImageFont.truetype('FreeSans.ttf', 12)
-large_font = ImageFont.truetype('FreeSans.ttf', 33)
 disp.fill(0)
 disp.show()
 
@@ -19,11 +17,10 @@ height = disp.height
 image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
 
-# Display a message on 3 lines, first line big font
-def display_message(top_line, line_2):
+# Display sensor reading on the OLED display
+def display_sensor_reading(sensor_reading):
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
-    draw.text((0, 0), top_line, font=large_font, fill=255)
-    draw.text((0, 50), line_2, font=small_font, fill=255)
+    draw.text((0, 0), sensor_reading, font=small_font, fill=255)
     disp.image(image)
     disp.show()
 
@@ -31,15 +28,11 @@ def display_message(top_line, line_2):
 sensor = DistanceSensor(echo=18, trigger=17)
 
 while True:
-    now = datetime.now()
-    date_message = '{:%d %B %Y}'.format(now)
-    time_message = '{:%H:%M:%S}'.format(now)
-    
     # Read distance from the sensor
     cm = sensor.distance * 100
     inch = cm / 2.5
-    distance_message = "Distance: {:.0f} cm / {:.0f} inches".format(cm, inch)
+    sensor_reading = "Distance: {:.0f} cm / {:.0f} inches".format(cm, inch)
 
-    # Display both time and distance on the OLED display
-    display_message(time_message, date_message + "\n" + distance_message)
+    # Display sensor reading on the OLED display
+    display_sensor_reading(sensor_reading)
     sleep(0.1)
