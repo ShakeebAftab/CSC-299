867875d9ce2b59e23bc1a522d42ef7dfbc36f1fd
shakeebaftab <shakeebaftab@gmail.com>
2024-02-13 15:25:18 -0600

    14.23

--- 14.23.py
+++ 14.23.py
@@ -1,31 +1,14 @@
 from gpiozero import DistanceSensor
 from time import sleep
-import VL53L1X
+from guizero import App, Text
 
-# Initialize VL53L1X sensor
-tof = VL53L1X.VL53L1X()
-tof.start_ranging(VL53L1X.VL53L1X_LONG_RANGE_MODE)
-
-# Initialize GPIO DistanceSensor
 sensor = DistanceSensor(echo=18, trigger=17)
 
-try:
-    while True:
-        # Read distance from HC-SR04
-        cm_sr04 = sensor.distance * 100
-        inch_sr04 = cm_sr04 / 2.54
+def update_reading():
+    cm = sensor.distance * 100
+    reading_text.value = str(cm)
 
-        # Read distance from VL53L1X
-        distance_vl53l1x = tof.get_distance()
-
-        # Display readings
-        print("HC-SR04: {:.0f} cm, {:.0f} inches | VL53L1X: {} mm".format(cm_sr04, inch_sr04, distance_vl53l1x))
-
-        sleep(0.5)
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    # Stop VL53L1X ranging
-    tof.stop_ranging()
+app = App(width=500, height=100)
+reading_text = Text(app, size=20)
+reading_text.repeat(1000, update_reading)
+app.display()
